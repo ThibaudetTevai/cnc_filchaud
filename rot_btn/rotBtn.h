75df4805c846f259ee946730f70f1ba642f68559
Tevaï THIBAUDET <tevai.thibaudet@gmail.com>
2019-10-28 22:49:00 +0100

    Lib Rot Btn work
    Signed-off-by: Tevaï THIBAUDET <tevai.thibaudet@gmail.com>

--- rot_btn/rotBtn.h
+++ rot_btn/rotBtn.h
@@ -3,15 +3,33 @@
 #ifndef ROTBTN_H_
 #define ROTBTN_H_
 
-void  	setupBtn(byte, byte, byte);
-void  	rotBtnRefresh();
+class RotBtn
+{
+  public:
+    RotBtn (uint8_t, uint8_t, uint8_t);
+    void    rotBtnRefresh();
+    bool    getPushedStatus();
+    bool    getCntUpdated();
+    uint8_t getValueRot();
+    bool    isUpdated();
+    
+	private:
+		void doDebounce();
+		void doEncoderRead();
+		void doUpdateCounter();
 
-bool  	getPushedStatus();
-bool  	getCntUpdated();
-uint8_t getValueRot();
+		bool valueA;
+		bool valueB;
+		bool btnPressed;
+		bool motionDetected;
+		bool cntUpdated;
+		bool CW;
 
-void  	doDebounce();
-void  	doEncoderRead();
-void  	doUpdateCounter();
+		uint8_t nettCounter;
+
+		Bounce debouncerA;
+		Bounce debouncerB;
+		Bounce debouncerSWITCH;
+};
 
 #endif
